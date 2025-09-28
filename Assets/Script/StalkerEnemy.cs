58b26366230c789375419111be8b6330c7d088ed
WDGH-TL <williamdantegarcia@gmail.com>
2025-09-27 22:43:27 -0300

    Animations Functional
    MiGo/Enemy now changes animations

--- Assets/Script/StalkerEnemy.cs
+++ Assets/Script/StalkerEnemy.cs
@@ -5,9 +5,26 @@ public class StalkerEnemy : MonoBehaviour
 {
     public NavMeshAgent miGo;
     public GameObject finalDestination;
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        if (agent == null || animator == null)
+        {
+            Debug.LogError("Faltan los componentes NavMeshAgent o Animator.");
+            enabled = false;
+        }
+    }
 
     public void Update()
     {
         miGo.destination = finalDestination.transform.position;
+        Vector3 worldVelocity = agent.velocity;
+        float speed = worldVelocity.magnitude;
+        animator.SetFloat("Speed", speed);
     }
 }
