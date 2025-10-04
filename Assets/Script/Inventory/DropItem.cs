45040e700336cb6618eb7b81a147d55e5fb1e175
WDGH-TL <williamdantegarcia@gmail.com>
2025-10-04 18:49:55 -0300

    DropItem
    Sound plays when item is dropped

--- Assets/Script/Inventory/DropItem.cs
+++ Assets/Script/Inventory/DropItem.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 
 public class DropItem : MonoBehaviour
 {
-    public AudioClip hitsound;
-    private AudioSource itemIsDropped;
-    public float minImpactForce = 0.5f;
+    public AudioSource itemIsDropped;
 
     void Start()
     {
         itemIsDropped = GetComponent<AudioSource>();
     }
 
-    private void OnCollisionEnter(Collision collision) // Prueba para sonido
+    public void OnCollisionEnter(Collision collision)
     {
-        float impactForce = collision.relativeVelocity.magnitude;
-
-        if (impactForce < minImpactForce && hitsound != null)
+        if (collision.transform.CompareTag("Floor"))
         {
-            float volume = Mathf.Clamp01(impactForce / 5f);
+            Debug.Log("Ayuda");
+            if (itemIsDropped != null && itemIsDropped.clip != null)
+            {
+                itemIsDropped.Play();
+            }
         }
     }
 }
